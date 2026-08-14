208c4fb48f8e9298575273cf101084499d8fbfee
Maarten Pronk <git@evetion.nl>
2026-08-14 19:33:48 +0200

    Add relative indexing.

--- src/DiscreteGlobalGrids.jl
+++ src/DiscreteGlobalGrids.jl
@@ -118,7 +118,8 @@ include("systems/A5/A5.jl")
 include("systems/S2/S2.jl")
 include("systems/ISEA4R/ISEA4R.jl")
 
-using .IGeo7: IGeo7System, Z7Cell
+using .IGeo7: IGeo7System, Z7Cell, HexIndex, RelativeIGEO7Index,
+    directioncode, trytranslate
 using .H3: H3System, H3Cell
 using .HEALPix: HEALPixSystem, HEALPixRingIndex
 using .A5: A5System, A5Cell
@@ -259,7 +260,8 @@ export CellLookup, Cells, Covering
 # `HierarchicalLevelGrid` from `levelgrid`. S2 and ISEA4R use `LevelIndex` over
 # their scaffold ordinals.
 export systems
-export IGeo7System, Z7Cell
+export IGeo7System, Z7Cell, HexIndex, RelativeIGEO7Index
+export directioncode, trytranslate
 export H3System, H3Cell
 export HEALPixSystem, HEALPixRingIndex
 export A5System, A5Cell

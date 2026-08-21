d69bd613bf31caecf0bdc41d11c499d0bd82cf70
Documenter.jl <documenter@juliadocs.github.io>
2026-08-21 06:58:16 +0000

    build based on 016df96

---
+++ previews/PR62/assets/all_dggs.md.Nw0WoHVH.lean.js
@@ -0,0 +1,25 @@
+import{_ as t,C as n,ao as h,o as e,c as k,ap as i,E as p,w as r,a2 as d,j as E}from"./chunks/framework.DOCLSC3r.js";const A=JSON.parse('{"title":"DGGS gallery","description":"","frontmatter":{},"headers":[],"relativePath":"all_dggs.md","filePath":"all_dggs.md","lastUpdated":null}'),g={name:"all_dggs.md"},y={class:"vp-raw-html",innerHTML:`<div><div class="bonito-fragment" id="c26e1e74-9026-45bd-ba45-c2df42274751" data-jscall-id="root"><div><script src="/DiscreteGlobalGrids.jl/previews/PR62/bonito/js/Bonito.bundled7919512299027874894.js" type="module"><\/script><style></style></div><div><script type="module">Bonito.init_session("c26e1e74-9026-45bd-ba45-c2df42274751", Bonito.fetch_binary('/DiscreteGlobalGrids.jl/previews/PR62/bonito/bin/fc622c2c5d4fac68deab2fe34ee4b3bf0ca2048c-12710573961591524894.bin'), "root", false);
+<\/script><div></div></div></div><div class="bonito-fragment" id="42a8abe2-097b-4ade-ac0e-42695928b6cf" data-jscall-id="subsession-application-dom"><div><style></style><style>.wglmakie-spinner {
+  left: 50%;
+  z-index: 1000;
+  height: 40px;
+  position: absolute;
+  border-radius: 50%;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  animation: wglmakie-spin 1.0s linear infinite;
+  top: 50%;
+  border-top: 4px solid currentColor;
+  width: 40px;
+  pointer-events: none;
+  transform: translate(-50%, -50%);
+}
+@keyframes wglmakie-spin {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style></div><div><script type="module">Bonito.init_session("42a8abe2-097b-4ade-ac0e-42695928b6cf", Bonito.fetch_binary('/DiscreteGlobalGrids.jl/previews/PR62/bonito/bin/a23775c46fc60a474584bee76c74cdd375b65d73-17695675051048582825.bin'), "sub", false);
+<\/script><div style="width: 100%; height: 100%; position: relative;" data-jscall-id="1"><canvas height="850px" data-jp-suppress-context-menu style="display: block" data-jscall-id="2" data-lm-suppress-shortcuts="true" width="1200px" tabindex="0"></canvas><div class="wglmakie-spinner" data-jscall-id="3"></div></div></div></div></div>`};function o(F,s,c,C,u,D){const a=n("ClientOnly"),l=h("exec-scripts");return e(),k("div",null,[s[0]||(s[0]=i("",3)),p(a,null,{default:r(()=>[d(E("div",y,null,512),[[l]])]),_:1}),s[1]||(s[1]=i("",7))])}const B=t(g,[["render",o]]);export{A as __pageData,B as default};

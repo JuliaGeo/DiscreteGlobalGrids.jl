2f5824ec157a58081929f4ba80b3614c512f04c0
Documenter.jl <documenter@juliadocs.github.io>
2026-09-09 03:34:07 +0000

    build based on bdda3d5

---
+++ previews/PR93/assets/index.md.CzStkySd.lean.js
@@ -0,0 +1,24 @@
+import{_ as e,C as l,ap as n,o as h,c as p,ao as i,E as k,w as r,a2 as d,j as o}from"./chunks/framework.HJs0P359.js";const v=JSON.parse('{"title":"DiscreteGlobalGrids.jl","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}'),g={name:"index.md"},E={class:"vp-raw-html",innerHTML:`<div class="bonito-fragment" id="db89c688-ce61-4f12-9fe4-62c93173c128" data-jscall-id="subsession-application-dom"><div><style></style><style>.wglmakie-spinner {
+  left: 50%;
+  z-index: 1000;
+  height: 40px;
+  position: absolute;
+  border-radius: 50%;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  animation: wglmakie-spin 1.0s linear infinite;
+  top: 50%;
+  border-top: 4px solid currentColor;
+  width: 40px;
+  pointer-events: none;
+  transform: translate(-50%, -50%);
+}
+@keyframes wglmakie-spin {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style></div><div><script type="module">Bonito.init_session("db89c688-ce61-4f12-9fe4-62c93173c128", Bonito.fetch_binary('/DiscreteGlobalGrids.jl/previews/PR93/bonito/bin/c35b82e408660dad9f1d17958302c9883464e676-12028720038537418274.bin'), "sub", false);
+<\/script><div style="width: 100%; height: 100%; position: relative;" data-jscall-id="4"><canvas height="640px" data-jp-suppress-context-menu style="display: block" data-jscall-id="5" data-lm-suppress-shortcuts="true" width="640px" tabindex="0"></canvas><div class="wglmakie-spinner" data-jscall-id="6"></div></div></div></div>`};function c(y,s,u,F,C,b){const a=l("ClientOnly"),t=n("exec-scripts");return h(),p("div",null,[s[0]||(s[0]=i("",4)),k(a,null,{default:r(()=>[d(o("div",E,null,512),[[t]])]),_:1}),s[1]||(s[1]=i("",24))])}const x=e(g,[["render",c]]);export{v as __pageData,x as default};

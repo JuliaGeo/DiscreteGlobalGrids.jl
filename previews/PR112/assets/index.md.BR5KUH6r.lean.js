8b1e0eb7da288e398578947fe2ec4a256c4c6b16
Documenter.jl <documenter@juliadocs.github.io>
2026-09-17 11:35:08 +0000

    build based on 0203df5

---
+++ previews/PR112/assets/index.md.BR5KUH6r.lean.js
@@ -0,0 +1,24 @@
+import{_ as e,C as l,ap as n,o as h,c as p,ao as s,E as r,w as k,a2 as d,j as o}from"./chunks/framework.Clk_7P8J.js";const m=JSON.parse('{"title":"DiscreteGlobalGrids.jl","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}'),g={name:"index.md"},c={class:"vp-raw-html",innerHTML:`<div data-jscall-id="subsession-application-dom" id="e10fcdcd-21ba-4051-9fdb-9af428eb2504" class="bonito-fragment"><div><style></style><style>.wglmakie-spinner {
+  animation: wglmakie-spin 1.0s linear infinite;
+  position: absolute;
+  left: 50%;
+  z-index: 1000;
+  border-radius: 50%;
+  width: 40px;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  transform: translate(-50%, -50%);
+  border-top: 4px solid currentColor;
+  height: 40px;
+  pointer-events: none;
+  top: 50%;
+}
+@keyframes wglmakie-spin {
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+}
+</style></div><div><script type="module">Bonito.init_session("e10fcdcd-21ba-4051-9fdb-9af428eb2504", Bonito.fetch_binary('/DiscreteGlobalGrids.jl/previews/PR112/bonito/bin/f1f1df3708ac4fdc60953a5054f36e63bedd0de8-12375487981700859258.bin'), "sub", false);
+<\/script><div data-jscall-id="4" style="width: 100%; height: 100%; position: relative;"><canvas data-jscall-id="5" height="640px" data-lm-suppress-shortcuts="true" width="640px" data-jp-suppress-context-menu tabindex="0" style="display: block"></canvas><div data-jscall-id="6" class="wglmakie-spinner"></div></div></div></div>`};function E(y,i,u,F,C,b){const a=l("ClientOnly"),t=n("exec-scripts");return h(),p("div",null,[i[0]||(i[0]=s("",4)),r(a,null,{default:k(()=>[d(o("div",c,null,512),[[t]])]),_:1}),i[1]||(i[1]=s("",29))])}const f=e(g,[["render",E]]);export{m as __pageData,f as default};
